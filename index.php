5ac1cf174e942757280a9e35fa52a0e40e9013db
Caio Alves <caiovictoralvesp@gmail.com>
2025-03-24 20:25:24 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+require 'functions.php';
+require 'Database.php';
+require 'Response.php';
+require 'router.php';
+
+// connect to MySQL database and execute a query
+$id = $_GET['id'];
+$query = "SELECT * FROM posts WHERE id = ?";
+
+$posts = $db -> query("$query", [$id])->fetch();
+
+dd($posts);
+
+
+//foreach($posts as $post) {
+//  echo "<li>" . $post['title'] . "</li>";
+//}
