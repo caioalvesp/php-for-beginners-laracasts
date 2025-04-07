d0a28100850aee4895f583920db81227d59afce2
Caio Alves <caiovictoralvesp@gmail.com>
2025-04-07 11:09:31 -0300

    added password encryptation with bcrypt and dynamic welcome message

--- controllers/registration/store.php
+++ controllers/registration/store.php
@@ -40,7 +40,7 @@ if ($user) {
   // if not, save one to the database, and then log them in, and redirect.
   $db->query('INSERT INTO users(email, password) VALUES(:email, :password)', [
     'email' => $email,
-    'password' => $password
+    'password' => password_hash($password, PASSWORD_BCRYPT)
   ]);
 
   //mark that the user is logged in.
