ac3130e631a12a85269ddf3bbe104706e4b2a472
Caio Alves <caiovictoralvesp@gmail.com>
2025-04-08 10:30:42 -0300

    refactored form login

--- Http/controllers/session/store.php
+++ Http/controllers/session/store.php
@@ -1,41 +1,21 @@
 <?php
 
-use Core\App;
-use Core\Database;
-use Core\Validator;
+use Core\Authenticator;
 use Http\Forms\LoginForm;
-
-$db = App::resolve(Database::class);
 
 $email = $_POST['email'];
 $password = $_POST['password'];
 
 $form = new LoginForm();
-$form->validate($email, $password);
 
-if (! $form->validate($email, $password)) {
-  return view('session/create.view.php', [
-    'errors' => $form->errors()
-  ]);
-}
-
-$user = $db->query('select * from users where email = :email', [
-  'email' => $email
-])->find();
-
-if ($user) {
-  if (password_verify($password, $user['password'])) {
-    login([
-      'email' => $email,
-    ]);
-
-    header('location: /');
-    exit();
+if ($form->validate($email, $password)) {
+  if ((new Authenticator)->attempt($email, $password)) {
+    redirect('/');
   }
+
+  $form->error('email', 'No matching account found for this email address and password.');
 }
 
 return view('session/create.view.php', [
-  'errors' => [
-    'email' => 'No matching account found for this email address and password.'
-  ]
+  'errors' => $form->errors()
 ]);
